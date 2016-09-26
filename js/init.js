4e52a5f6c9652d18fc70689ee5b893541c92beae
Niklas Johannesson <nj@searchminds.se>
2016-09-26 12:40:55 +0000

    like done

--- js/init.js
+++ js/init.js
@@ -3,6 +3,10 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.show-btn').click(function(){
+      $('.main-text').css("font-size","0px");
+      $('.hover-specific').css("margin","4px").css("text-transform", "uppercase");
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
